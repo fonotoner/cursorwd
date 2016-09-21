cecddc1c40b566c741d4e80a340b6580998210a8
hobetuz <hobetuz@ipartekaula.com>
2016-09-21 19:47:38 +0200

    Subida inical de archivos

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/*
+    @author: Ander Uraga Real
+    @version: 1.0
+    @date: 2016/09/19
+    Nuestro primer javascrpit
+*/
+
+//alert, desaconsejado
+//alert('Kaixo Gela');
+
+console.error('Muestra un mensaje de error');
+console.warn('Muestra un mensaje de aviso');
+console.info('Muestra un mensaje de informativo');
+console.log('Muestra un mensaje de traza');
